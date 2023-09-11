3c313407c1d2f342006f8a1d24de630c3d659226
Esen <esenbekm03@gmail.com>
2023-09-11 17:53:15 +0600

    lesson 1

---
+++ news/views.py
@@ -0,0 +1,55 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from news.models import News, Comment
+from news.serializers import NewsListSerializer, NewsDetailSerializer, CommentListSerializer
+
+
+@api_view(['GET'])
+def hello_world(request):
+    dct = {
+        'message': 'Hello, world!',
+        'int': 123,
+        'float': 123.456,
+        'list': [1, 2, 3],
+        'dict': {
+            'a': 1,
+            'b': 2,
+            'c': 3,
+        },
+        'bool': True
+    }
+
+    return Response(dct)
+
+
+@api_view(['GET'])
+def news_list(request):
+    # get all news
+    news = News.objects.all()
+    # SELECT * FROM news_news;
+
+    # serialize news
+    data = NewsListSerializer(instance=news, many=True).data
+
+    return Response(data)
+
+
+@api_view(['GET'])
+def news_detail(request, news_id):
+    try:
+        news = News.objects.get(id=news_id)
+    except News.DoesNotExist:
+        return Response({'ERROR': f"Новости с id {news_id} не существует"})
+
+    serializer = NewsDetailSerializer(instance=news, many=False)
+
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def comment_list(request):
+    comments = Comment.objects.all()
+    data = CommentListSerializer(instance=comments, many=True).data
+
+    return Response(data)
