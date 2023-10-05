1537cdd36c7af9586afc86c532a2fd336d63e2cd
Esen <esenbekm03@gmail.com>
2023-10-05 17:40:10 +0600

    added static

--- main/settings.py
+++ main/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config, Csv
 
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
